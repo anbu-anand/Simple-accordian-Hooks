199ab250ac0448ed453c6c397523445016f87eae
Anbarasan <37935894+anbuanand-github@users.noreply.github.com>
2020-12-22 17:27:08 +0530

    dropdown added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
@@ -12,10 +13,31 @@ const items = [
   }
 ];
 
+const options = [
+  {
+    label: "The color Red",
+    value: "red"
+  },
+  {
+    label: "The color Green",
+    value: "green"
+  },
+  {
+    label: "The color Blue",
+    value: "blue"
+  }
+];
+
 export default function App() {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 }
